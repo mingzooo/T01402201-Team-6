b848525d33f8da3c421b67e32a5e3a16bd1d1df7
mingzooo <joozzoo0223@gmail.com>
2023-06-04 18:25:39 +0900

    feat: 튜토리얼 화면

--- Assets/Scripts/BtnType.cs
+++ Assets/Scripts/BtnType.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BtnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public BTNType currentType;
-    public Vector3 buttonScale;
+    public Transform buttonScale;
     Vector3 defaultScale;
+
+    public GameObject tutorialScreen; // 튜토리얼 화면을 나타내는 UI 요소
+    public float fadeInDuration = 0.5f; // 페이드 인에 걸리는 시간
+
+    private CanvasGroup tutorialCanvasGroup; // 튜토리얼 화면의 캔버스 그룹
 
     private void Start()
     {
-        defaultScale = transform.localScale;
+        defaultScale = buttonScale.localScale;
+        tutorialScreen.SetActive(false);
+        tutorialCanvasGroup = tutorialScreen.GetComponent<CanvasGroup>();
+        tutorialCanvasGroup.alpha = 0f; // 초기에 알파 값을 0으로 설정하여 숨김
     }
 
     public void onBtnClick()
@@ -23,17 +32,36 @@ public class BtnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
                 break;
             case BTNType.Tutorial:
                 Debug.Log("게임 방법");
+                StartCoroutine(FadeInTutorialScreen());
                 break;
         }
     }
 
+    private IEnumerator FadeInTutorialScreen()
+    {
+        tutorialScreen.SetActive(true); // 튜토리얼 화면 활성화
+
+        tutorialCanvasGroup.alpha = 0f; // Set initial alpha to 0
+
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeInDuration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeInDuration);
+            tutorialCanvasGroup.alpha = alpha;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        tutorialCanvasGroup.alpha = 1f; // Ensure alpha value is set to 1
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        transform.localScale = defaultScale * 1.2f;
+        buttonScale.localScale = defaultScale * 1.2f;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        transform.localScale = defaultScale;
+        buttonScale.localScale = defaultScale;
     }
 }
