cc0fb26339d9d5be37801237de73ab9d3e4a1198
Dawon00 <dawon000203@gmail.com>
2023-06-08 23:23:37 +0900

    feat: 포탈 이동 조건 구현, stage_2 배경 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,117 +4,140 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed = 5f;
-    [SerializeField]
-    private float jumpPower = 5f;
+  [SerializeField]
+  private float moveSpeed = 5f;
+  [SerializeField]
+  private float jumpPower = 5f;
 
-    private Rigidbody2D playerRb;
-    private Animator playerAnim;
+  private Rigidbody2D playerRb; // 플레이어의 리지드바디
+  private Animator playerAnim;
 
-    private bool facingRight = true;
-    private bool isJumping = false;
-    private bool isRolling = false;
+  private bool facingRight = true;
+  private bool isJumping = false;
+  private bool isRolling = false;
 
-    // Start is called before the first frame update
-    void Start()
+  private void Awake()
+  {
+    playerRb = GetComponent<Rigidbody2D>();
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    playerRb = GetComponent<Rigidbody2D>();
+    playerAnim = GetComponent<Animator>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Z) && !isRolling)
     {
-        playerRb = GetComponent<Rigidbody2D>();
-        playerAnim = GetComponent<Animator>();
+      Shoot();
     }
 
-    // Update is called once per frame
-    void Update()
+    if (Input.GetKeyDown(KeyCode.X) && !isJumping && !isRolling)
     {
-        if (Input.GetKeyDown(KeyCode.Z) && !isRolling)
-        {
-            Shoot();
-        }
-
-        if (Input.GetKeyDown(KeyCode.X) && !isJumping && !isRolling)
-        {
-            Jump();
-        }
-
-        if (Input.GetKeyDown(KeyCode.C) && !isRolling && !isJumping)
-        {
-            StartCoroutine(Roll());
-        }
+      Jump();
     }
 
-    private void FixedUpdate()
+    if (Input.GetKeyDown(KeyCode.C) && !isRolling && !isJumping)
     {
-        if (!isRolling)
-        {
-            float move = Input.GetAxisRaw("Horizontal");
-            playerRb.velocity = new Vector2(move * moveSpeed, playerRb.velocity.y);
+      StartCoroutine(Roll());
+    }
+  }
 
-            if (move == 0f)
-                playerAnim.SetBool("Running", false);
-            else
-                playerAnim.SetBool("Running", true);
+  private void FixedUpdate()
+  {
+    if (!isRolling)
+    {
+      float move = Input.GetAxisRaw("Horizontal");
+      playerRb.velocity = new Vector2(move * moveSpeed, playerRb.velocity.y);
 
-            if (move > 0 && !facingRight)
-            {
-                Flip();
-            }
-            else if (move < 0 && facingRight)
-            {
-                Flip();
-            }
-        }
-    }
+      if (move == 0f)
+        playerAnim.SetBool("Running", false);
+      else
+        playerAnim.SetBool("Running", true);
 
-    private void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
+      if (move > 0 && !facingRight)
+      {
+        Flip();
+      }
+      else if (move < 0 && facingRight)
+      {
+        Flip();
+      }
     }
-    private void Shoot()
-    {
-        GameObject bullet = ObjectPooling.GetObject();
-        float dir = 1f;
-        if (!facingRight)
-            dir *= -1f;
-        Vector3 spawnPos = new Vector3(transform.position.x + dir * 0.7f, transform.position.y + 1.29f, transform.position.z);
-        bullet.transform.position = spawnPos;
-        bullet.GetComponent<BulletController>().Shoot(dir);
-    }
-    private void Jump()
-    {
-        playerRb.AddForce(Vector2.up*jumpPower, ForceMode2D.Impulse);
-    }
-    private IEnumerator Roll()
-    {
-        isRolling = true;
-        playerAnim.SetBool("Rolling", true);
-        if (facingRight)
-        {
-            playerRb.velocity = new Vector2(moveSpeed, playerRb.velocity.y);
-        }
-        else
-        {
-            playerRb.velocity = new Vector2(-moveSpeed, playerRb.velocity.y);
-        }
-        yield return new WaitForSeconds(0.5f);
-        playerAnim.SetBool("Rolling", false);
-        isRolling = false;
-    }
+  }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+  private void Flip()
+  {
+    facingRight = !facingRight;
+    Vector3 scale = transform.localScale;
+    scale.x *= -1;
+    transform.localScale = scale;
+  }
+  private void Shoot()
+  {
+    GameObject bullet = ObjectPooling.GetObject();
+    float dir = 1f;
+    if (!facingRight)
+      dir *= -1f;
+    Vector3 spawnPos = new Vector3(transform.position.x + dir * 0.7f, transform.position.y + 1.29f, transform.position.z);
+    bullet.transform.position = spawnPos;
+    bullet.GetComponent<BulletController>().Shoot(dir);
+  }
+  private void Jump()
+  {
+    playerRb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+  }
+  private IEnumerator Roll()
+  {
+    isRolling = true;
+    playerAnim.SetBool("Rolling", true);
+    if (facingRight)
     {
-        if (collision.gameObject.tag == "Ground")
-            isJumping = false;
-        else if (collision.gameObject.tag == "Bullet")
-            GameManager.Instance.PlayerDamaged(1);
+      playerRb.velocity = new Vector2(moveSpeed, playerRb.velocity.y);
     }
+    else
+    {
+      playerRb.velocity = new Vector2(-moveSpeed, playerRb.velocity.y);
+    }
+    yield return new WaitForSeconds(0.5f);
+    playerAnim.SetBool("Rolling", false);
+    isRolling = false;
+  }
 
-    private void OnCollisionExit2D(Collision2D collision)
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.tag == "Ground")
+      isJumping = false;
+    else if (collision.gameObject.tag == "Bullet")
+      GameManager.Instance.PlayerDamaged(1);
+  }
+
+  private void OnCollisionExit2D(Collision2D collision)
+  {
+    if (collision.gameObject.tag == "Ground")
+      isJumping = true;
+  }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    // 포탈에 닿으면
+    if (collision.gameObject.CompareTag("Finish"))
     {
-        if (collision.gameObject.tag == "Ground")
-            isJumping = true;
+      // 모든 적을 물리치면 다음 스테이지로
+      if (GameManager.Instance.CheckAllEnemiesDefeated())
+      {
+        GameManager.Instance.LoadNextStage();
+      }
+      // 적이 남아있으면
+      else
+      {
+        Debug.Log("Cannot proceed to the next stage. Defeat all enemies first!");
+      }
     }
+  }
 
 }
